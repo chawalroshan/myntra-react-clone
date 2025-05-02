2f69535d8d50265cc31790224579b2ad1fe2ce7d
Roshan <chawalroshan@gmail.com>
2025-05-02 21:29:23 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,32 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import HomeItem from './components/HomeItem'
 
 function App() {
   
+const item = {
+  id: '001',
+        image: '/images/image1.png',
+        company: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        original_price: 1045,
+        current_price: 606,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    };
 
   return (
     <>
      <Header/>
     <main>
         <div className="items-container">
+          <HomeItem item={item}/>
         </div>
     </main>
     <Footer/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { BsFillPersonFill } from "react-icons/bs";
+import { FaHeartbeat } from "react-icons/fa";
+import { IoBagCheckSharp } from "react-icons/io5";
+
 const Header = () => {
     return(
     <>
         <header>
         <div className="logo_container">
-            <a href="#"><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home"/></a>
+            <a href="#"><img className="myntra_home" src="/images/myntra_logo.png" alt="Myntra Home"/></a>
         </div>
         <nav className="nav_bar">
             <a href="#">Men</a>
@@ -19,17 +23,20 @@ const Header = () => {
         </div>
         <div className="action_bar">
             <div className="action_container">
-                <span className="material-symbols-outlined action_icon">person</span>
+                {/* <span className="material-symbols-outlined action_icon">person</span> */}
+                <BsFillPersonFill />
                 <span className="action_name">Profile</span>
             </div>
 
             <div className="action_container">
-                <span className="material-symbols-outlined action_icon">favorite</span>
+                {/* <span className="material-symbols-outlined action_icon">favorite</span> */}
+                <FaHeartbeat />
                 <span className="action_name">Wishlist</span>
             </div>
 
             <a className="action_container" href="pages/bag.html">
-                <span className="material-symbols-outlined action_icon">shopping_bag</span>
+                {/* <span className="material-symbols-outlined action_icon">shopping_bag</span> */}
+                <IoBagCheckSharp />
                 <span className="action_name">Bag</span>
                 <span className="bag-item-count">0</span>
             </a>
